97438cca004e85947a306c5eea55c027135f5d8e
kinghiee <superhiee@163.com>
2023-04-30 16:57:26 +0800

    build: 修改webpack配置

--- build/webpack.client.dev.cjs
+++ build/webpack.client.dev.cjs
@@ -5,84 +5,84 @@ const VUESSRCLIENTPLUGIN = require('vue-server-renderer/client-plugin')
 const WEBPACK_BASE_CONFIG = require('./webpack.base.cjs');
 
 module.exports = MERGE(WEBPACK_BASE_CONFIG, {
-    mode: 'development',
-    target: 'web', // web环境
-    entry: { app: RESOLVE(__dirname, '../client/entry-client.js') },
-    output: {
-      filename: '[name]_[contenthash:8].js',
-      path: RESOLVE(__dirname, '../dist'),
-      publicPath: '/'
-    },
-    cache: {
-      type: 'filesystem',
-      name: 'devClientCache-development',
-      idleTimeoutAfterLargeChanges: 1000,
-      maxAge: 604800, // 允许未使用的缓存留在文件系统缓存中的时间
-    },
-    module: {
-        rules: [
-            {
-              oneOf: [
-                {
-                  test: /\.css$/i,
-                  use: [
-                      {
-                        loader: 'style-loader',
-                      },
-                      {
-                        loader: 'css-loader',
-                        options: {
-                          importLoaders: 1,
-                        },
-                      },
-                      'postcss-loader',
-                  ],
+  mode: 'development',
+  target: 'web', // web环境
+  entry: { app: RESOLVE(__dirname, '../client/entry-client.js') },
+  output: {
+    filename: '[name]_[contenthash:8].js',
+    path: RESOLVE(__dirname, '../dist'),
+    publicPath: '/'
+  },
+  cache: {
+    type: 'filesystem',
+    name: 'devClientCache-development',
+    idleTimeoutAfterLargeChanges: 1000,
+    maxAge: 604800, // 允许未使用的缓存留在文件系统缓存中的时间
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.css$/i,
+            use: [
+              {
+                loader: 'style-loader',
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
                 },
-                {
-                  test: /\.less$/i,
-                  exclude: /node_modules/,
-                  use: [
-                    { loader: 'vue-style-loader', options: { ssrId: false  } },
-                    {
-                      loader: 'css-loader',
-                      options: {
-                        importLoaders: 1,
-                      }
-                    },
-                    'postcss-loader',
-                    'less-loader',
-                  ],
-                },
-              ]
-            }
+              },
+              'postcss-loader',
+            ],
+          },
+          {
+            test: /\.less$/i,
+            exclude: /node_modules/,
+            use: [
+              { loader: 'vue-style-loader', options: { ssrId: false  } },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                }
+              },
+              'postcss-loader',
+              'less-loader',
+            ],
+          },
         ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      // 优化:代码分割
+      chunks: 'all',
+      automaticNameDelimiter: '~',
+      minChunks: 2,
+      cacheGroups: {
+      vendor: {
+          // 第三方依赖
+          priority: 1, // 设置优先级，首先抽离第三方模块
+          name: 'vendor',
+          test: /node_modules/,
+          chunks: 'initial',
+          minSize: 0,
+          minChunks: 1, // 最少引入了1次
+          },
+      },
     },
-    optimization: {
-        splitChunks: {
-            // 优化:代码分割
-            chunks: 'all',
-            automaticNameDelimiter: '~',
-            minChunks: 2,
-            cacheGroups: {
-            vendor: {
-                // 第三方依赖
-                priority: 1, // 设置优先级，首先抽离第三方模块
-                name: 'vendor',
-                test: /node_modules/,
-                chunks: 'initial',
-                minSize: 0,
-                minChunks: 1, // 最少引入了1次
-                },
-            },
-        },
-        runtimeChunk: {
-            name: (entrypoint) => `runtime~${entrypoint.name}`,
-        },
+    runtimeChunk: {
+      name: (entrypoint) => `runtime~${entrypoint.name}`,
     },
-    plugins: [
-        new VUELOADERPLUGIN(),
-        new VUESSRCLIENTPLUGIN({
-            filename: 'vue-ssr-client-manifest.json'
-        }),
-    ],
+  },
+  plugins: [
+      new VUELOADERPLUGIN(),
+      new VUESSRCLIENTPLUGIN({
+        filename: 'vue-ssr-client-manifest.json'
+      }),
+  ],
 });
--- build/webpack.server.dev.cjs
+++ build/webpack.server.dev.cjs
@@ -6,62 +6,62 @@ const NODEEETERNALS = require('webpack-node-externals');
 const WEBPACK_BASE_CONFIG = require('./webpack.base.cjs');
 
 module.exports = MERGE(WEBPACK_BASE_CONFIG, {
-    mode: 'development',
-    target: 'node',
-    devtool: 'eval-cheap-source-map',
-    entry: RESOLVE(__dirname, '../client/entry-server.js'),
-    output: {
-        filename: 'server-bundle.js',
-        path: RESOLVE(__dirname, '../dist'),
-        libraryTarget: 'commonjs2'
-    },
-    cache: {
-      type: 'filesystem',
-      name: 'devServerCache-development',
-      idleTimeoutAfterLargeChanges: 1000,
-      maxAge: 604800, // 允许未使用的缓存留在文件系统缓存中的时间
-    },
-    module: {
-        rules: [
-            {
-              oneOf: [
-                  {
-                    test: /\.css$/i,
-                    use: [
-                        {
-                          loader: 'style-loader',
-                        },
-                        {
-                          loader: 'css-loader',
-                          options: { importLoaders: 1 }
-                        },
-                        'postcss-loader',
-                    ],
-                  },
-                  {
-                    test: /\.less$/i,
-                    exclude: /node_modules/,
-                    use: [
-                      {
-                        loader: 'css-loader',
-                        options: {
-                          importLoaders: 1,
-                        }
-                      },
-                      'postcss-loader',
-                      'less-loader',
-                    ],
-                  },
-              ]
-            }
+  mode: 'development',
+  target: 'node',
+  devtool: 'eval-cheap-source-map',
+  entry: RESOLVE(__dirname, '../client/entry-server.js'),
+  output: {
+    filename: 'server-bundle.js',
+    path: RESOLVE(__dirname, '../dist'),
+    libraryTarget: 'commonjs2'
+  },
+  cache: {
+    type: 'filesystem',
+    name: 'devServerCache-development',
+    idleTimeoutAfterLargeChanges: 1000,
+    maxAge: 604800, // 允许未使用的缓存留在文件系统缓存中的时间
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.css$/i,
+            use: [
+              {
+                loader: 'style-loader',
+              },
+              {
+                loader: 'css-loader',
+                options: { importLoaders: 1 }
+              },
+              'postcss-loader',
+            ],
+          },
+          {
+            test: /\.less$/i,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                }
+              },
+              'postcss-loader',
+              'less-loader',
+            ],
+          },
         ]
-    },
-    externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
-    externals: [NODEEETERNALS()], // in order to ignore all modules in node_modules folder
-    plugins: [
-        new VUELOADERPLUGIN(),
-        new VUESERVERPlUGINSSR({
-            filename: 'vue-ssr-server-bundle.json'
-        }),
+      }
     ]
+  },
+  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  externals: [NODEEETERNALS()], // in order to ignore all modules in node_modules folder
+  plugins: [
+    new VUELOADERPLUGIN(),
+    new VUESERVERPlUGINSSR({
+      filename: 'vue-ssr-server-bundle.json'
+    }),
+  ]
 });
